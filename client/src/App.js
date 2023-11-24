426e61ebfcf1f8de7f19ea3cb2252b3e7ec88b29
MD SALIM <mdsalim.iiitr@gmail.com>
2023-11-24 19:37:00 +0530

    Done basic setup for Signup and Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {LoginPage, SignupPage} from "./Routes.js";
 
-function App() {
+const App = () => {
   return (
-    <div>
-    <div className="text-[100px]" >Hello Salim</div>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/sign-up" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </>  
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/SignupPage";
+
+const Routes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export {LoginPage, SignupPage}
