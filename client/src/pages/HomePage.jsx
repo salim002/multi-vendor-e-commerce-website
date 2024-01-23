cbb82b938b4f2904fd86161a5860901b12697b23
MD SALIM <mdsalim.iiitr@gmail.com>
2024-01-23 12:40:07 +0530

    Completed Footer section for home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,10 @@ import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
+import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
+import Events from "../components/Route/Events/Events";
+import Sponsered from "../components/Route/Sponsored";
+import Footer from "../components/Layout/Footer";
 
 const HomePage = () => {
   return (
@@ -11,6 +15,10 @@ const HomePage = () => {
       <Hero/>
       <Categories/>
       <BestDeals/>
+      <Events/>
+      <FeaturedProduct/>
+      <Sponsered/>
+      <Footer/>
     </div>
   )
 }
