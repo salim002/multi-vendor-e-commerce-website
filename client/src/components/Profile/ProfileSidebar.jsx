95a2ad48e73a188e4b44e16cb8008cd8fac0ec46
MD SALIM <mdsalim.iiitr@gmail.com>
2024-01-25 12:56:22 +0530

    created a protected route to protect user profile page

--- client/src/components/Profile/ProfileSidebar.jsx
+++ client/src/components/Profile/ProfileSidebar.jsx
@@ -9,9 +9,25 @@ import { MdOutlineTrackChanges } from "react-icons/md";
 import { TbAddressBook } from "react-icons/tb";
 import { RxPerson } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
 
 const ProfileSidebar = ({ setActive, active }) => {
   const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    axios
+      .get(`${server}/user/logout`, { withCredentials: true })
+      .then((res) => {
+        toast.success(res.data.message);
+        window.location.reload(true);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
+  };
 
   return (
     <div className="w-full bg-white shadow-sm rounded-[10px] p-4 pt-8">
@@ -82,7 +98,7 @@ const ProfileSidebar = ({ setActive, active }) => {
 
       <div
         className="flex items-center cursor-pointer w-full mb-8"
-        onClick={() => setActive(8)}
+        onClick={() => setActive(8) || logoutHandler()}
       >
         <AiOutlineLogin size={20} color={active === 8 ? "red" : ""} />
         <span className={`pl-3 ${active === 8 ? "text-[red]" : ""}`}>
