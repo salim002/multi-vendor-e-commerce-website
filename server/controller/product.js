64065abbda96ae1651ca6266449e2858d2e1fc92
MD SALIM <mdsalim.iiitr@gmail.com>
2024-04-07 12:40:29 +0530

    Completed Review functionality for users

--- server/controller/product.js
+++ server/controller/product.js
@@ -6,6 +6,7 @@ const { isSeller, isAuthenticated, isAdmin } = require("../middleware/auth");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Product = require("../model/product");
 const Shop = require("../model/shop");
+const Order = require("../model/order");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { upload } = require("../multer");
 
@@ -109,6 +110,64 @@ router.delete(
     }
   })
 );
+
+// review for a product
+router.put(
+  "/create-new-review",
+  isAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { user, rating, comment, productId, orderId } = req.body;
+
+      const product = await Product.findById(productId);
+
+      const review = {
+        user,
+        rating,
+        comment,
+        productId,
+      };
+
+      const isReviewed = product.reviews.find(
+        (rev) => rev.user._id === req.user._id
+      );
+
+      if (isReviewed) {
+        product.reviews.forEach((rev) => {
+          if (rev.user._id === req.user._id) {
+            (rev.rating = rating), (rev.comment = comment), (rev.user = user);
+          }
+        });
+      } else {
+        product.reviews.push(review);
+      }
+
+      let avg = 0;
+
+      product.reviews.forEach((rev) => {
+        avg += rev.rating;
+      });
+
+      product.ratings = avg / product.reviews.length;
+
+      await product.save({ validateBeforeSave: false });
+
+      await Order.findByIdAndUpdate(
+        orderId,
+        { $set: { "cart.$[elem].isReviewed": true } },
+        { arrayFilters: [{ "elem._id": productId }], new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "Reviwed succesfully!",
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
 
 
 module.exports = router;
